fcad7b5e455705f10d8bcd183ae8bc5358bf0f44
minhdq <d.quangminh1991@gmail.com>
2019-11-06 21:01:03 +0700

    initial commit

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    void Update()
+    {
+        Debug.Log(player.position);
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 500f;
+    public float sideForce = 200f;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
